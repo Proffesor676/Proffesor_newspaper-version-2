65d11b24f3f66b055785bdf8890ac37707c54a17
Proffesor676 <116460661+Proffesor676@users.noreply.github.com>
2023-01-19 17:49:42 +0300

    version with access
    Version with access to different groups (auth for example), can be changed in admin panel. Also added buttoms with urls with functions on the main page.

--- newspaper/news/views.py
+++ newspaper/news/views.py
@@ -3,8 +3,10 @@ from .filters import PostFilter
 from .models import Post
 from django.urls import reverse_lazy
 from .forms import PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
-class PostList(ListView):
+class PostList(LoginRequiredMixin, ListView):
     model = Post
     ordering = 'dateCreation'
     template_name = 'posts.html'
@@ -20,7 +22,7 @@ class PostList(ListView):
         context['filterset'] = self.filterset
         return context
 
-class PostsSearch(ListView):
+class PostsSearch(LoginRequiredMixin, ListView):
     model = Post
     ordering = 'title'
     template_name = 'news_search.html'
@@ -42,41 +44,47 @@ class PostDetail(DetailView):
     context_object_name = 'post'
 
 
-class PostCreate(CreateView):
+class PostCreate(PermissionRequiredMixin, CreateView):
+    permission_required = ('news.add_post',)
     form_class = PostForm
     model = Post
-    template_name = 'post_edit.html'
+    template_name = 'post_create.html'
     def form_valid(self, form):
         post = form.save(commit=False)
         post.categoryType = 'NW'
         return super().form_valid(form)
 
-class ArticleCreate(CreateView):
+class ArticleCreate(PermissionRequiredMixin, CreateView):
+    permission_required = ('news.add_article',)
     form_class = PostForm
     model = Post
-    template_name = 'article_edit.html'
+    template_name = 'article_create.html'
     def form_valid(self, form):
         article = form.save(commit=False)
         article.categoryType = 'AR'
         return super().form_valid(form)
 
-class PostUpdate(UpdateView):
+class PostUpdate(PermissionRequiredMixin, UpdateView):
+    permission_required = ('news.change_post',)
     form_class = PostForm
     model = Post
     template_name = 'post_edit.html'
 
-class PostDelete(DeleteView):
+class PostDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = ('news.delete_post',)
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('post_list')
 
 
-class ArticleUpdate(UpdateView):
+class ArticleUpdate(PermissionRequiredMixin, UpdateView):
+    permission_required = ('news.change_article',)
     form_class = PostForm
     model = Post
     template_name = 'article_edit.html'
 
-class ArticleDelete(DeleteView):
+class ArticleDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = ('news.delete_article',)
     model = Post
     template_name = 'article_delete.html'
     success_url = reverse_lazy('post_list')
--- newspaper/newspaper/urls.py
+++ newspaper/newspaper/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
    path('admin/', admin.site.urls),
-   path('pages/', include('django.contrib.flatpages.urls')),
+   path("accounts/", include("accounts.urls")),
+   path("accounts/", include("allauth.urls")),
    path('posts/', include('news.urls')),
 ]
